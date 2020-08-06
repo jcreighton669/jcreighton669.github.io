8459d5592299882e1890bf87973455b85fa8de3f
Justin Allen Creighton <46484674+jcreighton669@users.noreply.github.com>
2020-08-06 16:25:38 -0400

    Add files via upload

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,8 +1,22 @@
 function accordionDrop(id) {
-  var x = document.getElementById(id);
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
-  } else {
-    x.className = x.className.replace(" w3-show", "");
-  }
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+myIndex = 0;
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {
+        myIndex = 1;
+    }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 2000);
 }
